cec5a9bc8e367ef6255a44b1366481e96cfd936d
kwekwenwei <kwekww-wa16@student.tarc.edu.my>
2018-12-31 17:56:48 +0800

    Initial commit

---
+++ app/src/main/java/my/edu/taruc/lab42database/User.java
@@ -0,0 +1,49 @@
+package my.edu.taruc.lab42database;
+
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.PrimaryKey;
+import android.support.annotation.NonNull;
+
+@Entity(tableName = "user")
+public class User {
+    @PrimaryKey
+    @NonNull
+    private String firstName;
+    private String phone;
+    private String lastName;
+
+    public User(){
+
+    }
+
+    public User(@NonNull String firstName, String phone, String lastName) {
+        this.firstName = firstName;
+        this.phone = phone;
+        this.lastName = lastName;
+    }
+
+    @NonNull
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(@NonNull String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
